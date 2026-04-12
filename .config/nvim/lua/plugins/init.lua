0b86a7b1bd8aa8a08db638db277a53c073836650
xi <village.cowherd@gmail.com>
2026-04-12 18:57:36 +0800

    feat: 配置快捷键提示

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,21 +1,22 @@
 -- require("3rdlibs.lazy")
 vim.pack.add({
-    "https://github.com/ellisonleao/gruvbox.nvim",
-    "https://github.com/edeneast/nightfox.nvim",
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/stevearc/conform.nvim",
-    { src = "https://github.com/saghen/blink.cmp",      version = "v1" },
-    { src = "https://github.com/neovim/nvim-lspconfig", version = "v2.7.0" },
-    { src = "https://github.com/folke/snacks.nvim",     version = "v2.31.0" },
-    { src = "https://github.com/nvim-mini/mini.nvim",   version = "v0.17.0" },
-    { src = "https://github.com/mason-org/mason.nvim",  version = "v2.2.1" },
+	"https://github.com/ellisonleao/gruvbox.nvim",
+	"https://github.com/edeneast/nightfox.nvim",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/stevearc/conform.nvim",
+	"https://github.com/folke/which-key.nvim",
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1" },
+	{ src = "https://github.com/neovim/nvim-lspconfig", version = "v2.7.0" },
+	{ src = "https://github.com/folke/snacks.nvim", version = "v2.31.0" },
+	{ src = "https://github.com/nvim-mini/mini.nvim", version = "v0.17.0" },
+	{ src = "https://github.com/mason-org/mason.nvim", version = "v2.2.1" },
 
-    { src = "https://github.com/mfussenegger/nvim-dap", version = "0.10.0" },
-    { src = "https://github.com/nvim-neotest/nvim-nio", version = "v1.10.1" },
-    { src = "https://github.com/rcarriga/nvim-dap-ui",  version = "v4.0.0" }
+	{ src = "https://github.com/mfussenegger/nvim-dap", version = "0.10.0" },
+	{ src = "https://github.com/nvim-neotest/nvim-nio", version = "v1.10.1" },
+	{ src = "https://github.com/rcarriga/nvim-dap-ui", version = "v4.0.0" },
 })
 
-require("gruvbox").setup {}
+require("gruvbox").setup({})
 
 vim.o.background = "dark"
 vim.cmd.colorscheme("gruvbox")
@@ -29,4 +30,5 @@ require("plugins.snacks")
 require("plugins.mason")
 require("plugins.mini")
 require("plugins.dap")
+require("plugins.which_key")
 require("plugins.packui")
